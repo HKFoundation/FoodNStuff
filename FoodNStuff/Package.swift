c550b47c968df094ffe7348899e22a15d898bdd6
liusen <liusen@xbotgo.com>
2025-04-14 18:12:18 +0800

    提交代码

---
+++ FoodNStuff/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FoodNStuff",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FoodNStuff",
+            targets: ["FoodNStuff"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FoodNStuff"),
+        .testTarget(
+            name: "FoodNStuffTests",
+            dependencies: ["FoodNStuff"]
+        ),
+    ]
+)
---
+++ FoodNStuff/Sources/FoodNStuff/FoodItem.swift
@@ -0,0 +1,14 @@
+//
+//  FoodItem.swift
+//  FoodNStuff
+//
+//  Created by ✐ ᵕ̈ ᴹᴼᴿᴺᴵᴺᴳ on 2025/4/14.
+//
+
+import Foundation
+
+//注意不同module访问需要将访问权限设为pubilc
+public struct FoodItem {
+    public var title = "FoodItem"
+    public init(){}
+}
